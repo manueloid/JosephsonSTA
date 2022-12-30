ef4233cf4481c81d5fead6ff2e359ee9ef536cdb
manuel <manuel.odelli@gmail.com>
2022-12-30 11:57:23 +0000

    First Working example
    Added the ControlParameter struct and exported it in
    `src/JosephsonSTA.jl`.
    Added DataFrames to the list of packages in Project.toml

--- src/JosephsonSTA.jl
+++ src/JosephsonSTA.jl
@@ -1,5 +1,14 @@
 module JosephsonSTA
 
+export ControlParameter
 # Write your package code here.
+# Fundamental structure of the whole code is the ControlParameter one that contains all the values I need for the calculation.
+struct ControlParameter
+    ω0::Float64
+    ωf::Float64
+    final_time::Float64
+    NParticles::Int64
+end 
 
 end
+using DataFrames
